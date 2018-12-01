1d5c2c50e2fd24c361325c342c187ac89e4d83cf
Elytrus <moses@mosesxu.net>
2018-12-01 17:42:16 -0500

    added available meetings

--- PTIO/views.py
+++ PTIO/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http.response import Http404
 from PTIO.models.profile import ParentProfile, TeacherProfile, StudentProfile
-from PTIO.models.meeting import TeacherClass
+from PTIO.models.meeting import TeacherClass, MeetingTimeslot
 
 
 # Create your views here.
@@ -18,8 +18,27 @@ def index(request):
 def parent(request, parent_id):
     try:
         parent_obj = ParentProfile.objects.get(pk=parent_id)
-        children = StudentProfile.objects.filter(parent_account_host__pk=parent_id)
+        children = StudentProfile.objects.filter(parent_account_host__pk=parent_id).all()
+        children_ids = map(lambda child: child.id, children)
+        classes = []
         meetings = []
+
+        for curr_class in TeacherClass.objects.all():
+            relevant = False
+
+            for student in curr_class.students.all():
+                if student.id in children_ids:
+                    relevant = True
+                    break
+
+            if relevant:
+                classes.append(curr_class)
+
+        for meeting in MeetingTimeslot.objects.all():
+            for curr_class in classes:
+                if meeting.teacher_class.id == curr_class.id:
+                    meetings.append((curr_class, meeting))
+                    break
 
         return render(request, 'PTIO/parent.html', {'parent_id': parent_id, 'parent_obj': parent_obj,
                                                     'children': children, 'meetings': meetings})
@@ -33,8 +52,10 @@ def teacher(request, teacher_id):
         for class_obj in TeacherClass.objects.filter(teacher__pk=teacher_id).all():
             classes.append((class_obj, class_obj.students.all()))
 
+        slots = MeetingTimeslot.objects.filter(teacher_class__teacher__pk=teacher_id)
+
         return render(request, 'PTIO/teacher.html', {'teacher_id': teacher_id, 'teacher_obj': teacher_obj,
-                                                     'classes': classes})
+                                                     'classes': classes, 'slots': slots})
     except TeacherProfile.DoesNotExist:
         raise Http404('Invalid teacher id!')
 
