a6a07a439e1a5b1067cc054e1e7efc06f069ec04
Jonathan Sumabat <diaxaxx@gmail.com>
2018-12-01 17:51:09 -0500

    Fix plural form of teacher class

--- PTIO/models/meeting.py
+++ PTIO/models/meeting.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from .profile import TeacherProfile, StudentProfile
+from django.utils.translation import ugettext_lazy as _
 
 SUBJECT_VALIDATORS = [RegexValidator(regex='^[a-zA-Z ]+$', message='Subject can only contain letters and spaces')]
 
@@ -13,6 +14,10 @@ class TeacherClass(models.Model):
     def __str__(self):
         return '%s\'s %s class' % (self.teacher, self.subject)
 
+    class Meta:
+        verbose_name = _('teacher class')
+        verbose_name_plural = _('teacher classes')
+        
 
 class MeetingTimeslot(models.Model):
     teacher_class = models.ForeignKey(TeacherClass, on_delete=models.CASCADE)
