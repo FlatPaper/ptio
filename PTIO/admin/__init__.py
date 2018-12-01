e7ad95625255d2d15b1950de582f9c05fb813c35
Jonathan Sumabat <diaxaxx@gmail.com>
2018-12-01 13:40:28 -0500

    Add Teacher Profile
    this is so sad

--- PTIO/admin/__init__.py
+++ PTIO/admin/__init__.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from PTIO.admin.profile import StudentProfileAdmin, ParentProfileAdmin
-from PTIO.models import StudentProfile, ParentProfile
+from PTIO.admin.profile import StudentProfileAdmin, ParentProfileAdmin, TeacherProfileAdmin
+from PTIO.models import StudentProfile, ParentProfile, TeacherProfile
 
 admin.site.register(StudentProfile, StudentProfileAdmin)
 admin.site.register(ParentProfile, ParentProfileAdmin)
+admin.site.register(TeacherProfile, TeacherProfileAdmin)
