f6826559f2e3a45416a62018886e5d267eae4b20
Elytrus <moses@mosesxu.net>
2018-12-01 18:34:56 -0500

    added date string

--- PTIO/urls.py
+++ PTIO/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('parent/<int:parent_id>', views.parent),
     path('teacher/<int:teacher_id>', views.teacher),
     path('student/', views.student),
-    path('slot/register/<int:parent_id>/<int:slot_id>/', views.register_slot),
-    path('slot/register/complete_registration', views.register_slot_post)
+    path('slot/register/<int:parent_id>/<int:slot_id>/', views.register_slot, name='register'),
+    path('slot/register/complete_registration', views.register_slot_post, name='complete_registration')
 ]
--- PTIO/views.py
+++ PTIO/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http.response import Http404
+from django.urls import reverse
+from django.http.response import Http404, HttpResponseRedirect
 from PTIO.models.profile import ParentProfile, TeacherProfile, StudentProfile
 from PTIO.models.meeting import TeacherClass, MeetingTimeslot, UsedTimeSlot
+import datetime
 
 
 # Create your views here.
@@ -71,9 +73,34 @@ def register_slot(request, parent_id, slot_id):
                                                        'used': already_used})
 
 
+def str_to_datetime(date_string):
+    spl = date_string.split('T')
+    y, m, d = map(int, spl[0].split('-'))
+    h, m = map(int, spl[1].split(':'))
+    return datetime.datetime(y, m, d, h, m, 0)
+
+
 def register_slot_post(request):
-    parent = ParentProfile.objects.get(pk=request['parentid'])
-    slot = MeetingTimeslot.objects.get(pk=request['slotid'])
+    parent = ParentProfile.objects.get(pk=request.POST['parentid'])
+    slot = MeetingTimeslot.objects.get(pk=request.POST['slotid'])
+    usedslots = UsedTimeSlot.objects.get(pk=slot.id)
+
+    curstart = str_to_datetime(request.POST['start-time'])
+    curend = str_to_datetime(request.POST['end-time'])
+
+    if curstart < slot.start_time or curend > slot.end_time:
+        return render(request, 'PTIO/register_slot.html', {'err': 'Chosen time slot not within parent slot bounds!'})
+
+    works = True
+    for usedslot in usedslots:
+        if curend > usedslot.start_time or curstart < usedslot.end_time:
+            works = False
+            break
+
+    if works:
+        return render(request, 'PTIO/register_slot.html', {'err': 'Time slot submitted!'})
+    else:
+        return render(request, 'PTIO/register_slot.html', {'err': 'Chosen time slot is already taken!'})
 
 
 def student(request):
